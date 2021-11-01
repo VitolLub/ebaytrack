9f8cd238f39f659bd65f4036602462c225e759fe
VitolLub <VitolLub@gmail.com>
2021-11-01 14:35:27 +0200

    Update test_run.py

--- cron/test_run.py
+++ cron/test_run.py
@@ -3,7 +3,7 @@ print('Hello ')
 import requests
 from bs4 import BeautifulSoup as soup
 
-url = "https://www.aliexpress.com/item/1005003393871894.html?spm=a2g0o.productlist.0.0.64303178LJ4Z7q&algo_pvid=f8ab3789-1762-45cb-a355-5801d661ddf8&algo_exp_id=f8ab3789-1762-45cb-a355-5801d661ddf8-0&pdp_ext_f=%7B%22sku_id%22%3A%2212000025581510125%22%7D"
+url = "https://www.aliexpress.com/item/1005003393871894.html"
 session = requests.Session()
 session.cookies.set('Host', 'aliexpress.com', domain='.aliexpress.com', path='/')
 session.cookies.set('region', 'US', domain='.aliexpress.com', path='/')
@@ -19,9 +19,14 @@ proxyDict = {
 
 response = requests.get(url, timeout=20, headers=headers, proxies=proxyDict)
 res = soup(response.content, features="lxml")
-print(response.encoding )
-print(response.headers)
-print(response.cookies)
+
 print(res.find("title"))
 
-print(response.content)
+#print(response.content)
+full_script_data = res.find_all("script")
+for full_script in full_script_data:
+    try:
+        script = full_script.getText()
+        print(script[:500])
+    except:
+        pass
