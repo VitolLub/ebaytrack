45aadfa6fd171d3f1f174fcfab6d8a60a8e35cd7
VitolLub <VitolLub@gmail.com>
2021-11-01 16:21:52 +0200

    Update test_run.py

--- cron/test_run.py
+++ cron/test_run.py
@@ -2,8 +2,9 @@ print('Hello ')
 
 import requests
 from bs4 import BeautifulSoup as soup
+import json
 
-url = "https://www.aliexpress.com/item/1005003393871894.html"
+url = "https://aliexpress.ru/item/1005001999800710.html?spm=a2g0o.productlist.0.0.1e993178FMvmFE&algo_pvid=ccb3dbae-7e5b-44f5-a8de-4c4b624d098f&algo_exp_id=ccb3dbae-7e5b-44f5-a8de-4c4b624d098f-2&pdp_ext_f=%7B%22sku_id%22%3A%2212000018368782106%22%7D"
 session = requests.Session()
 session.cookies.set('Host', 'aliexpress.com', domain='.aliexpress.com', path='/')
 session.cookies.set('region', 'US', domain='.aliexpress.com', path='/')
@@ -21,14 +22,18 @@ response = requests.get(url, timeout=20, headers=headers, proxies=proxyDict)
 res = soup(response.content, features="lxml")
 
 print(res.find("title"))
-
+title = res.find("title")
 #print(response.content)
-full_script_data = res.find_all("script")
-for full_script in full_script_data:
-    try:
-        res = full_script.getText()
-        if res.find('dress')>0:
-            print("----- cript -----")
-            print(res)
-    except:
-        pass
+# full_script_data = res.find_all("script")
+# for full_script in full_script_data:
+#     res = full_script.getText()
+#     if res.find('window.runParams')>0:
+#         # print("----- cript -----")
+#         # y = json.loads(res)
+#         print(res)
+
+
+print('__AER_DATA__')
+full_script_data = res.find("script", {'id': '__AER_DATA__'})
+res = full_script_data.getText()
+print(res)
