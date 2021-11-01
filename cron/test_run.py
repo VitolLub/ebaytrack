56b947729722e91c4311a3c7aae33d0360092cfd
VitolLub <VitolLub@gmail.com>
2021-11-01 19:24:01 +0200

    Run test code on server

--- cron/test_run.py
+++ cron/test_run.py
@@ -1,10 +1,7 @@
-print('Hello ')
-
 import requests
 from bs4 import BeautifulSoup as soup
-import json
 
-url = "https://aliexpress.ru/item/1005001999800710.html?spm=a2g0o.productlist.0.0.1e993178FMvmFE&algo_pvid=ccb3dbae-7e5b-44f5-a8de-4c4b624d098f&algo_exp_id=ccb3dbae-7e5b-44f5-a8de-4c4b624d098f-2&pdp_ext_f=%7B%22sku_id%22%3A%2212000018368782106%22%7D"
+url = "https://www.aliexpress.ru/item/1005003393871894.html?c_tp=RUB&region=UK&b_locale=en_US"
 session = requests.Session()
 session.cookies.set('Host', 'aliexpress.com', domain='.aliexpress.com', path='/')
 session.cookies.set('region', 'US', domain='.aliexpress.com', path='/')
@@ -17,21 +14,13 @@ headers = {
 proxyDict = {
               "http"  : "http://213.230.69.193:3128"
             }
-
-response = requests.get(url, timeout=20, headers=headers, proxies=proxyDict)
+a = {'aep_usuc_f':'region=AU&site=glo&b_locale=en_US&c_tp=USD'}
+response = requests.get(url, timeout=20, headers=headers, cookies=a)
 res = soup(response.content, features="lxml")
-
+print(response.encoding )
+print(response.headers)
+print(response.cookies)
 print(res.find("title"))
-title = res.find("title")
-#print(response.content)
-# full_script_data = res.find_all("script")
-# for full_script in full_script_data:
-#     res = full_script.getText()
-#     if res.find('window.runParams')>0:
-#         # print("----- cript -----")
-#         # y = json.loads(res)
-#         print(res)
-
 
 print('__AER_DATA__')
 full_script_data = res.find("script", {'id': '__AER_DATA__'})
